4b8b537e4097db8b673a63c18d279b19a535e78c
Mizu <71164002+MiyagawaMizu@users.noreply.github.com>
2024-10-13 01:53:26 +0700

    Update and rename no_dev.js to sub.js

---
+++ scripts/sub.js
@@ -0,0 +1,26 @@
+// Can't inspect element, contextmenu
+
+// document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+// function ctrlShiftKey(e, keyCode) {
+// 	return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// }
+
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+
+// document.onkeydown = (e) => {
+// 	if (
+// 		event.keyCode === 123 ||
+// 		ctrlShiftKey(e, "I") ||
+// 		ctrlShiftKey(e, "J") ||
+// 		ctrlShiftKey(e, "C") ||
+// 		(e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+// 	) {
+// 		return false;
+// 	}
+// };
+
+// lazy loading
+for (var i = 0; i < images.length; i++) {
+	images[i].setAttribute("loading", "lazy");
+}
